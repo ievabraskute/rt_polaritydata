b7582cefb64fe3aa9f7670354205533d42e80f13
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-01 14:21:32 +0000

    Adding tarfile member sanitization to extractall()

--- polarity_svm_xgboost.py
+++ polarity_svm_xgboost.py
@@ -36,7 +36,29 @@ with tarfile.open("rt-polaritydata.tar.gz", "r") as f:
     # check what's in the file
     print(f.getmembers())
     # and extract the data
-    f.extractall()
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(f)
 
 
 # In[3]:
